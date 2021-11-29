4eeedcf683a54bcb5cb52823084204be95fbf6f8
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2021-11-29 13:36:08 -0800

    added tests for isEven

--- code.js
+++ code.js
@@ -12,5 +12,12 @@ function sayHello(input) {
 }
 
 function isFive(input) {
-    return input === 5 || input === "5";
+    return input == 5;
+}
+function isEven(input) {
+    if(input == 0) {
+        return false;
+    } else {
+        return input % 2 == 0;
+    }
 }
--- tests.js
+++ tests.js
@@ -53,3 +53,37 @@ describe('isFive', function () {
         expect(isFive("5")).toBe(true);
     });
 })
+describe('isEven', function () {
+    it('should be a defined function', function () {
+        expect(typeof isEven).toBe("function");
+    });
+    it('should return a boolean when called', function() {
+        expect(typeof isEven()).toBe("boolean");
+    });
+    it('isEven(2) should return true when executed', function() {
+        expect(isEven(2)).toBe(true);
+    });
+    it('isEven(-4) should return true when executed', function() {
+        expect(isEven(-4)).toBe(true);
+    });
+    it('isEven(3) should return false when executed', function() {
+        expect(isEven(3)).toBe(false);
+    });
+    it('isEven("banana") should return false when executed', function() {
+        expect(isEven("banana")).toBe(false);
+    });
+    it('isEven("8") should return true when executed', function() {
+        expect(isEven("8")).toBe(true);
+    });
+    it('isEven(Infinity) should return false when executed', function() {
+        expect(isEven(Infinity)).toBe(false);
+    });
+    it('isEven(true) and isEven(false) should return false when executed', function() {
+        expect(isEven(true)).toBe(false);
+    } && function() {
+        expect(isEven(false)).toBe(false);
+    });
+    it('isEven() should return false when executed', function() {
+        expect(isEven()).toBe(false);
+    });
+})
